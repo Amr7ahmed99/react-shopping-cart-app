f94b173d24217516c3bbb6f046b63c268396516f
Amr7ahmed99 <amr7ahmed99@gmail.com>
2022-06-30 14:00:07 +0200

    file structure

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,9 +1,20 @@
 //Top Level Functions (task, src, dest, watch)
 
-import { task, watch, src, dest } from 'gulp';
-import gulpSass from 'gulp-sass';
+// import { task, watch, src, dest } from 'gulp';
+// import gulpSass from 'gulp-sass';
+// const sass= gulpSass(require('sass'));
+
+const gulp = require('gulp');
+const gulpSass= require('gulp-sass');
 const sass= gulpSass(require('sass'));
 
+
+
+gulp.task('sass', ()=>{
+    gulp.watch('', async()=>{
+        gulp.src('src/Sass.scss').pipe(sass()).pipe(gulp.dest('src'));
+    });
+});
 // gulp.task('log', async ()=>{
 //     console.log("Logggg");
 // });
@@ -13,9 +24,8 @@ const sass= gulpSass(require('sass'));
 // });
 
 
-task('sass', async()=>{
-    watch('src/*.scss', async()=>{
-        src('src/*.scss').pipe(sass()).pipe(dest('src'));
-    });
-
-});
+// task('sass', async()=>{
+//     watch('src/*.scss', async()=>{
+//         src('src/*.scss').pipe(sass()).pipe(dest('src'));
+//     });
+// });
