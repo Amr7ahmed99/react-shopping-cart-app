3b1578459d4f48fea0ebc5f0e30924a37bf30cd0
Amr7ahmed99 <amr7ahmed99@gmail.com>
2022-07-12 19:01:41 +0200

    add data json and words js files

--- client/src/components/Local/header/Header.js
+++ client/src/components/Local/header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../../../css/header/Header.css';
+import { words } from '../../../words';
 export default function Header() {
   return (
     <header>
-        Shopping Cart
+        {words.headerTitle}
     </header>
   )
 }
---
+++ client/src/words.js
@@ -0,0 +1,5 @@
+export const words= {
+    headerTitle: "Shopping Cart",
+    contentTitle: "Contnet",
+    footerTitle: "Footer"
+};
