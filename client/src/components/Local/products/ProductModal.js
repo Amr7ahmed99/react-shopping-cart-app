32bf87fd865da75ad696951ade79f4dd8d880920
Amr7ahmed99 <amr7ahmed99@gmail.com>
2022-07-13 19:54:41 +0200

    product modal to display product info

---
+++ client/src/components/Local/products/ProductModal.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+import Modal from 'react-modal';
+export default function ProductModal(props) {
+    const {productInfo, closeModal}= props;//Destructing
+    
+  return (
+    <Modal isOpen= {productInfo} onRequestClose= {closeModal}>
+        <span className='close-Icon' onClick= {()=>closeModal()}> &times;</span>
+        <div className='product-Modal'>
+            <img src= {productInfo.imageUrl} alt= {productInfo.title}/>
+            <div className='product-desc'>
+                <p> {productInfo.title}</p>
+                <p> {productInfo.price}</p>
+            </div>
+        </div>
+    </Modal>
+  )
+}
