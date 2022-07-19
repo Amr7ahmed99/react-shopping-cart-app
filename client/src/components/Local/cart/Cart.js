7f78e94f2de7df41a31fcd7e02be53dc95969b16
Amr7ahmed99 <amr7ahmed99@gmail.com>
2022-07-19 12:34:44 +0200

    update checkout  animation

--- client/src/components/Local/cart/Cart.js
+++ client/src/components/Local/cart/Cart.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import '../../../css/cart/Cart.css';
 import Checkout from '../checkout/Checkout';
+import Fade from 'react-reveal/Fade'
 export default function Cart({cartItems, removeFromCart}) {
   const [showForm, setShowForm]= useState(false);
   const [values, setValues]= useState("");
@@ -16,33 +17,38 @@ export default function Cart({cartItems, removeFromCart}) {
       name: values.name,
       email: values.email
     }
+    // console.log(order);
   }
   return (
     <section className='cart-wrapper'>
         <div className='cart-title'> 
           <p> {cartItems.length} Items In Cart</p> 
         </div>
-        <div className='cart-items'>
-          {
-            cartItems.map( item => (
-              <div className='cart-item' key= {item.id}>
-                <div className='item-info'>
-                  <img src= {item.image} alt= {item.title} className='item-image'/>
-                  <div>
-                    <p> <b>item:</b> {item.title}</p>
-                    <p> <b>Q:</b> {item.quantity}</p>
-                    <p> <b>price:</b> ${item.price}</p>
+        <Fade left cascade>
+            <div className='cart-items'>
+              {
+                cartItems.map( item => (
+                  <div className='cart-item' key= {item.id}>
+                    <div className='item-info'>
+                      <img src= {item.image} alt= {item.title} className='item-image'/>
+                      <div>
+                        <p> <b>item:</b> {item.title}</p>
+                        <p> <b>Q:</b> {item.quantity}</p>
+                        <p> <b>price:</b> ${item.price}</p>
+                      </div>
+                    </div>
+                    <div className='remove-item'>
+                      <button type= "button" role= "button" onClick={()=> removeFromCart(item.id)}>
+                        Remove
+                      </button>
+                    </div>
                   </div>
-                </div>
-                <div className='remove-item'>
-                  <button type= "button" role= "button" onClick={()=> removeFromCart(item.id)}>
-                    Remove
-                  </button>
-                </div>
-              </div>
-            ))
-          }
-        </div>
+
+                ))
+              }
+            </div>
+        </Fade>
+
         <div className='cart-checkout'>
           <div className='total-price'>
             <p> 
