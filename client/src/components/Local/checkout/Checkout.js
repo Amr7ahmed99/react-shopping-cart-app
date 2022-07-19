6313668df092bb1369e1b1696cf50a627d4da203
Amr7ahmed99 <amr7ahmed99@gmail.com>
2022-07-19 12:10:15 +0200

    adding animation to filter, cart and  checkout components

--- client/src/components/Local/checkout/Checkout.js
+++ client/src/components/Local/checkout/Checkout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Fade from "react-reveal/Fade"
 import '../../../css/checkout/Checkout.css';
 export default function Checkout({showForm, setShowForm, onSubmitOrder, handelChange}) {
   return (
@@ -8,20 +9,22 @@ export default function Checkout({showForm, setShowForm, onSubmitOrder, handelCh
             &&
             (
             <div className='checkout-form'>
-                <form method= "GET" action= "#" onSubmit={onSubmitOrder}>
-                <span className= "close-checkout" onClick={()=> setShowForm(false)}> &times;</span>
-                <div className='form-group'>
-                    <label htmlFor='name'> Name</label>
-                    <input type= "text" onChange={(e)=> handelChange(e)} required name= "name" id="name"/>
-                </div>
-                <div className='form-group'>
-                    <label htmlFor='email'> Email</label>
-                    <input type= "email" onChange={(e)=> handelChange(e)} required name= "email" id="email"/>
-                </div>
-                <div className='form-group'>
-                    <button type= "submit" role= "button"> Checkout </button>
-                </div>
-                </form>
+                <Fade left >
+                    <form method= "GET" action= "#" onSubmit={onSubmitOrder}>
+                    <span className= "close-checkout" onClick={()=> setShowForm(false)}> &times;</span>
+                    <div className='form-group'>
+                        <label htmlFor='name'> Name</label>
+                        <input type= "text" onChange={(e)=> handelChange(e)} required name= "name" id="name"/>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='email'> Email</label>
+                        <input type= "email" onChange={(e)=> handelChange(e)} required name= "email" id="email"/>
+                    </div>
+                    <div className='form-group'>
+                        <button type= "submit" role= "button"> Checkout </button>
+                    </div>
+                    </form>
+                </Fade>
             </div>
             )
         }
