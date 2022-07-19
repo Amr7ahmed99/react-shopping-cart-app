861e19cb88209540d8db74874f18c9a524e6e84f
Amr7ahmed99 <amr7ahmed99@gmail.com>
2022-07-19 11:49:33 +0200

    adding bounce animation to product

--- client/src/components/Local/products/Products.js
+++ client/src/components/Local/products/Products.js
@@ -1,7 +1,7 @@
 import React, { useState , useEffect} from 'react';
 import '../../../css/products/Products.css';
 import ProductModal from './ProductModal';
-
+import Bounce from 'react-reveal/Bounce';
 
 
 export default function Products({products, productModal, openModal, closeModal, addToCart}) {
@@ -13,16 +13,19 @@ export default function Products({products, productModal, openModal, closeModal,
                         &&
                     products.map(product => {
                         return (
-                            <div key= {product.id} className= "product-item">
-                                <img src= {product.image} alt= {product.title} onClick= {()=> openModal(product)}/>
-                                <div className='product-desc'>
-                                    <p> {product.title}</p>
-                                    <p> ${product.price}</p>
-                                </div>
-                                <p> rating: {product.rating.rate}</p>
+                            <Bounce bottom cascade key= {product.id}>
+                                <div  className= "product-item">
+                                    <img src= {product.image} alt= {product.title} onClick= {()=> openModal(product)}/>
+                                    <div className='product-desc'>
+                                        <p> {product.title}</p>
+                                        <p> ${product.price}</p>
+                                    </div>
+                                    <p> rating: {product.rating.rate}</p>
 
-                                <button className='AddToCart' onClick= {()=> addToCart(product)} type='button' role= "button"> Add To Cart</button>
-                            </div>
+                                    <button className='AddToCart' onClick= {()=> addToCart(product)} type='button' role= "button"> Add To Cart</button>
+                                </div>
+                            </Bounce>
+
                             
                         );
                     })
